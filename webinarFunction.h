677e87cac5e664dc3e2c87bb0a452cfce1ee4ce8
Tubagus Dylan Rachmat <tubagus.dylanr@gmail.com>
2022-04-05 13:30:51 +0700

    nambah panduan fungsi

--- webinarFunction.h
+++ webinarFunction.h
@@ -4,5 +4,9 @@
 
 void adminMenu(char [][50], char [][50], char[][10], int);
 void guestMenu(char [][50], char [][50], char[][10], int);
+void panduanGuestMenu();
+void panduanInfoWebinar();
+void panduanDaftarWebinar();
+void panduanSearchWebinar();
 
 #endif
