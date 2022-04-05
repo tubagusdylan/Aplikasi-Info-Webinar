41836ab9614eb426cd5036f99e51284f30a0cf98
Tubagus Dylan Rachmat <tubagus.dylanr@gmail.com>
2022-04-05 18:50:37 +0700

    Update webinarFunction.h

--- webinarFunction.h
+++ webinarFunction.h
@@ -4,6 +4,7 @@
 
 void adminMenu(char [][50], char [][50], char[][10], int);
 void guestMenu(char [][50], char [][50], char[][10], int);
+void searchWebinar(char [][50]);
 void panduanGuestMenu();
 void panduanInfoWebinar();
 void panduanDaftarWebinar();
